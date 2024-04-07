b8a7e9535581817956aff1baf7424c3bb674baea
masamiru <162574177+masamiru@users.noreply.github.com>
2024-04-07 13:49:49 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+let nowDate = new Date();
+
+let year = nowDate.getFullYear();
+let month = nowDate.getMonth() + 1;
+let day = nowDate.getDate();
+
+console.log(year + '年' + month + '月' + day + '日');
